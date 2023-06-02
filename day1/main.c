c2dfd37d8e842ee78735ae0013d8907e8a6054cf
Witold Zolnowski <blue3656@gmail.com>
2023-06-02 21:49:08 +0100

    day1 - getting string length to traverse in reverse, but failing at the string len estimation

--- day1/main.c
+++ day1/main.c
@@ -2,13 +2,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int get_string_size(char* s, int max_size);
+
 int main() {
   FILE * fp;
-  int bufferLength = 255;
+  int bufferLength = 50;
   char buffer[bufferLength];
+
   fp = fopen("input", "r");
   while(fgets(buffer, bufferLength, fp)){
-    printf("%s",buffer);
+    int str_size = get_string_size(buffer, bufferLength);
+    printf("size of this line is %i\n", str_size);
+    printf("last char is %c\n", buffer[str_size-1]);
+    buffer[str_size] = '\0';
+    printf("entire string is %s", buffer);
+    break;
   }
   fclose(fp);
+}
+
+int snafu_map(char n){
+  switch(n){
+    case '2':
+      return 2;
+    case '1':
+      return 1;
+    case '0':
+      return 0;
+    case '-':
+      return -1;
+    case '=':
+      return -2;
+  }
+  return -9; // error
+}
+
+int get_string_size(char* s, int max_size){
+  int index = 0;
+  for(int i=0; i< max_size; i++, index++){
+    if(s[i] == '\n'){
+      return index;
+    }
+  }
+  return 0;
 }

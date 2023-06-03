e8741784bd27347f8f5de050b93bc83e45aa3512
Witold Zolnowski <blue3656@gmail.com>
2023-06-03 21:41:52 +0100

    day1 - refactor to use actual input, returning wrong answer

--- day1/main.cpp
+++ day1/main.cpp
@@ -11,30 +11,35 @@ int main() {
   FILE *fp;
   int bufferLength = 50;
   char buffer[bufferLength];
+  int result = 0;
 
   fp = fopen("input", "r");
   while (fgets(buffer, bufferLength, fp)) {
     // int str_size = get_string_size(buffer, bufferLength);
-    char s[] = "1=-0-2";
-    int str_size = strlen(s);
-    double result = snafu_to_int(s, str_size);
-    printf("result is %f", result);
-    printf("should be %i", 1747);
-    break;
+    // char s[] = "2=0=";
+    int str_size = strlen(buffer)-1;
+    // printf("str len is : %i\n", str_size);
+    // printf("char at 17 is %i\n", buffer[16]);
+    result += snafu_to_int(buffer, str_size);
   }
   fclose(fp);
+  printf("result is %i", result);
 }
 
 double snafu_to_int(char *s, int size) {
   double result=0;
   double snafu_at_position_i;
-  for (int i = size; i > 0; i--) {
-    double base_value = pow(5, i);
-    char snafu_char = s[i - 1];
+  for (int i = size, y = 0; i > 0; i--, y++) {
+    double base_value = pow(5, y);
+    char snafu_char = s[i-1];
     int snafu_int = snafu_map(snafu_char);
     snafu_at_position_i =  base_value * snafu_int;
     result += snafu_at_position_i;
   }
+  printf("\n");
+  printf("string is : %s\n", s);
+  printf("snafu value is : %f\n", result);
+  printf("=====================\n");
   return result;
 }
 

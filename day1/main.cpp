7af94300e209a488244177e38bf00b523a9d0b6a
Witold Zolnowski <blue3656@gmail.com>
2023-06-04 06:12:39 +0100

    day1 - mock decimal to snafu function

--- day1/main.cpp
+++ day1/main.cpp
@@ -1,35 +1,45 @@
-#include <string.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int get_string_size(char *s, int max_size);
-long snafu_to_int(char *s, int size);
+long snafu_to_int(char *s);
 int snafu_map(char n);
+void decimal_to_snafu(long n, char *output);
 
 int main() {
   FILE *fp;
   int bufferLength = 50;
   char buffer[bufferLength];
-  long result = 0;
+  long snafu_total = 0;
 
   fp = fopen("input", "r");
   while (fgets(buffer, bufferLength, fp)) {
-    int str_size = strlen(buffer)-1;
-    result += snafu_to_int(buffer, str_size);
+    snafu_total += snafu_to_int(buffer);
   }
+  char result_as_snafu[100];
+  decimal_to_snafu(snafu_total, result_as_snafu);
   fclose(fp);
-  printf("result is %ld\n", result);
+  printf("result is %ld\n", snafu_total);
+  printf("expected : %ld\n", 32005641587247);
+  printf("snafu total : %s\n", result_as_snafu);
 }
 
-long snafu_to_int(char *s, int size) {
-  long double result=0;
+void decimal_to_snafu(long input, char *output) {
+  char foo[] = "wow";
+  output = foo;
+}
+
+long snafu_to_int(char *s) {
+  int size = strlen(s) - 1; // strlen counts \n, so we remove it from calc
+  long double result = 0;
   double snafu_at_position_i;
   for (int i = size, y = 0; i > 0; i--, y++) {
     double base_value = pow(5, y);
-    char snafu_char = s[i-1];
+    char snafu_char = s[i - 1];
     int snafu_int = snafu_map(snafu_char);
-    snafu_at_position_i =  base_value * snafu_int;
+    snafu_at_position_i = base_value * snafu_int;
     result += snafu_at_position_i;
   }
   printf("\n");

62100c8d0e153e1f0c7fb9679294222aa91d8cfe
Witold Zolnowski <blue3656@gmail.com>
2023-06-11 09:54:28 +0100

    d1 end of part 1

--- d1/main.cpp
+++ d1/main.cpp
@@ -1,4 +1,5 @@
 #include <bits/types/FILE.h>
+#include <cstdlib>
 #include <cstring>
 #include <limits.h>
 #include <stdio.h>
@@ -13,17 +14,26 @@ int main() {
   if (fp == NULL) {
     exit(1);
   }
-  int elf_calories[3000];
-  int counter = 0;
+  long long elf_calories_arr[3000] = {0};
+  long long calorie_counter = 0;
+  long long arr_pointer = 0;
   while ((read = getline(&line, &len, fp)) != -1) {
-    if (counter == 10){
-      break;
-    }
-    counter++;
-    if(!strcmp(line, "\n")) {
-      printf("new line !\n");
+    if (!strcmp(line, "\n")) {
+      elf_calories_arr[arr_pointer] = calorie_counter;
+      arr_pointer++;
+      calorie_counter = 0;
     } else {
-      printf("%s", line);
+      long long n = atoi(line);
+      calorie_counter += n;
     }
   }
+  long long max;
+  long long elf;
+  for (int i = 0; i < 3000; i++) {
+    if (elf_calories_arr[i] > max) {
+      max = elf_calories_arr[i];
+      elf = i + 1;
+    }
+  }
+  printf("Max calories is %lld, elf %lld\n", max, elf);
 }

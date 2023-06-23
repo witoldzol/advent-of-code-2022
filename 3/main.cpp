df00619dff4b2f2317937d518047739903d478b8
Witold Zolnowski <blue3656@gmail.com>
2023-06-23 06:16:40 +0100

    3,1 read input

--- 3/main.cpp
+++ 3/main.cpp
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main(){
-  printf("hi\n");
-  return 1;
+  FILE *fh = fopen("input", "r");
+
+  size_t buffer_size = 255;
+  char buffer[buffer_size];
+  while(fgets(buffer, buffer_size, fh)){
+    printf("%s", buffer);
+  }
 }

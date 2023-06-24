86bce86e5b5f6d9f4427aaf2a7952301b4f33e42
Witold Zolnowski <blue3656@gmail.com>
2023-06-24 21:48:52 +0100

    4,1 add sample input, iterate over file

--- 4/main.cpp
+++ 4/main.cpp
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
-int main(){
-  printf("heloo");
+int main() {
+  FILE *fp = fopen("sample_input", "r");
+  size_t buffer_size = 255;
+  char buffer[buffer_size];
+  while (fgets(buffer, buffer_size, fp)) {
+    printf("%s", buffer);
+  }
 }

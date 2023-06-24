814c22ad80875399406fb3bfaa706ba86c5f0eec
Witold Zolnowski <blue3656@gmail.com>
2023-06-24 21:50:36 +0100

    4,1 assert sample result

--- 4/main.cpp
+++ 4/main.cpp
@@ -1,10 +1,16 @@
 #include <stdio.h>
+#include <assert.h>
 
-int main() {
-  FILE *fp = fopen("sample_input", "r");
+int get_overlaps(char *file_name){
+  FILE *fp = fopen(file_name, "r");
   size_t buffer_size = 255;
   char buffer[buffer_size];
   while (fgets(buffer, buffer_size, fp)) {
     printf("%s", buffer);
   }
+  return 2;
+}
+
+int main() {
+  assert(get_overlaps("sample_input") == 2);
 }

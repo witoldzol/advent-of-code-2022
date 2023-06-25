63866b2a74021e717194d4fb022f42e71e032474
Witold Zolnowski <blue3656@gmail.com>
2023-06-25 07:23:22 +0100

    extra - create lib for stack data structure

---
+++ lib/ds.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/* my datastructures */
+typedef struct Stack {
+  int *data;
+} Stack;
+
+int push(int *stack, int n);
+int pop(int *stack, int n);
+int top(int *stack);
+
+
+int push(Stack stack, int n){
+  
+}
+
+int main(){
+  printf("bob");
+}

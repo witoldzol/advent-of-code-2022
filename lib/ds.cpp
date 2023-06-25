612e4156195f182d196e6c8a3100eea24d77a5ae
Witold Zolnowski <blue3656@gmail.com>
2023-06-25 08:47:05 +0100

    extra - add push & pop functions

--- lib/ds.cpp
+++ lib/ds.cpp
@@ -1,19 +1,49 @@
+#include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /* my datastructures */
 typedef struct Stack {
+  int top = -1;
+  int size;
   int *data;
 } Stack;
 
-int push(int *stack, int n);
+// int push(int *stack, int n);
 int pop(int *stack, int n);
 int top(int *stack);
 
-
-int push(Stack stack, int n){
-  
+int push(Stack *stack, int n) {
+  if (stack->top >= stack->size + 1) {
+    printf("Stack overflow! Stack size if = [ %d ], current top = [ %d ]\n",
+           stack->size, stack->top);
+    exit(1);
+  }
+  stack->top++;
+  stack->data[stack->top] = n;
+  printf("[INFO] Inserted %d at index %d\n", n, stack->top);
+  printf("[INFO] Value at index %d = %d\n", stack->top,
+         stack->data[stack->top]);
+  return n;
 }
 
-int main(){
-  printf("bob");
+int pop(Stack *stack) {
+  if (stack->top < 0) {
+    printf("Stack underflow! Stack size if = [ %d ], current top = [ %d ]\n",
+           stack->size, stack->top);
+    exit(1);
+  }
+  int temp = stack->data[stack->top];
+  stack->data[stack->top] = 0;
+  printf("[INFO] Popped %d at index %d\n", temp, stack->top);
+  stack->top--;
+  return temp;
+}
+
+int main() {
+  int *data = (int *)malloc(4 * sizeof(int));
+  Stack mystack = {.size = 4, .data = data};
+  push(&mystack, 4);
+  int r = pop(&mystack);
+  // printf("popped === %d", r);
 }

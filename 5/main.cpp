b69061a67b8859c5a131110f750cd917975dac5f
Witold Zolnowski <blue3656@gmail.com>
2023-06-25 22:29:19 +0100

    5,1 parse sample input

--- 5/main.cpp
+++ 5/main.cpp
@@ -1,6 +1,36 @@
-#include <stdio.h>
+#include <cstring>
 #include "stack.h"
+#include <bits/types/FILE.h>
+#include <cstdlib>
+#include <stdio.h>
 
-int main(){
-  printf("hello");
+/* sample input
+    [D]
+[N] [C]
+[Z] [M] [P]
+ 1   2   3
+ */
+int main() {
+  char s1[100] = {'Z', 'N'};
+  Stack stack1 = {.top = 1, .size = 100, .data = s1};
+  char s2[100] = {'M', 'C', 'D'};
+  Stack stack2 = {.top = 2, .size = 100, .data = s2};
+  char s3[100] = {'P'};
+  Stack stack3 = {.top = 0, .size = 100, .data = s3};
+
+  FILE *fh = fopen("sample_input", "r");
+  int buffer_size = 255;
+  char buffer[buffer_size];
+  while (fgets(buffer, buffer_size, fh)) {
+    printf("line: %s", buffer);
+    char *_ = strtok(buffer, " ");
+    int move = atoi(strtok(NULL, " "));
+    printf("move: %d\n", move);
+    char *__ = strtok(NULL, " ");
+    int from = atoi(strtok(NULL, " "));
+    printf("from: %d\n", from);
+    char *___ = strtok(NULL, " ");
+    int to = atoi(strtok(NULL, " "));
+    printf("to: %d\n", to);
+  }
 }
--- 5/stack.h
+++ 5/stack.h
@@ -1,3 +1,9 @@
-int push(int *stack, int n);
-int pop(int *stack);
-int top(int *stack);
+typedef struct Stack {
+  int top = -1;
+  int size;
+  char *data;
+} Stack;
+
+char push(Stack *stack, char n);
+char pop(Stack *stack);
+char top(Stack *stack);
